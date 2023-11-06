554a175a1ebb129a806c9345045c9360b3f7a5f1
Jean Espindola <espindolaJV@outlook.com>
2023-11-06 17:04:54 -0300

    <Commit das 2 primeiras caixas de texto para ver posicionamento>

---
+++ resources/components/CampaignPage/Textos_curtos.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <input type="text" v-model="texto" placeholder="Título do RPG" style="width: 651px; height: 60px; font-size: 20px;"><br><br><br>
+      <input type="text" v-model="texto" placeholder="Tema do RPG" style="width: 651px; height: 60px; font-size: 20px;">
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        texto: ''
+      };
+    }
+  };
+  </script>
