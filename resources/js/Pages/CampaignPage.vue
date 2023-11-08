3aa2d86761a670154e2c0d79a50e5f31bee09dde
Jean Espindola <espindolaJV@outlook.com>
2023-11-08 01:44:20 -0300

    <Adição das caixas de texto na página de campanha>

--- resources/js/Pages/CampaignPage.vue
+++ resources/js/Pages/CampaignPage.vue
@@ -1,6 +1,9 @@
 <script setup>
 import CapaUploader from '../../components/CampaignPage/CapaUploader.vue';
 import NavBar from '../../components/NavBar.vue';
+import Textos_curtos from '../../components/CampaignPage/Textos_curtos.vue';
+import Texto_Lore from '../../components/CampaignPage/Texto_Lore.vue';
+import Textos_grandes from '../../components/CampaignPage/Textos_grandes.vue';
 
 defineProps({
     currentPage: String,
